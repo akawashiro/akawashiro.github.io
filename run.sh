a9ba07d577f82d01d0a6262551d347d0eb0d981a
Akira Kawata <akirakawata1@gmail.com>
2024-10-13 10:25:10 +0900

    Fix IP addr

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ IMAGE_NAME=akawashiro_site_image
 
 EXITING_CONTAINER_ID=$(sudo podman ps -aqf "name=akawashiro_site_container")
 if [ -n "${EXITING_CONTAINER_ID}" ]; then
-  podman stop ${EXITING_CONTAINER_ID}
-  podman rm ${EXITING_CONTAINER_ID}
+  sudo podman stop ${EXITING_CONTAINER_ID}
+  sudo podman rm ${EXITING_CONTAINER_ID}
 fi
-podman pull docker.io/library/nginx:latest
-podman image build --network host . -t ${IMAGE_NAME}
-podman run -d --name ${CONTAINER_NAME} ${IMAGE_NAME}
-CONTAINER_IP_ADDR=$(podman inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_NAME})
+sudo podman pull docker.io/library/nginx:latest
+sudo podman image build --network host . -t ${IMAGE_NAME}
+sudo podman run -d --name ${CONTAINER_NAME} ${IMAGE_NAME}
+CONTAINER_IP_ADDR=$(sudo podman inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_NAME})
 echo "Access to http://${CONTAINER_IP_ADDR}"
