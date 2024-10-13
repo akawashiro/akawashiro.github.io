49f943f62bb9f069df704edf147dc9bc41495e37
Akira Kawata <akirakawata1@gmail.com>
2024-10-13 10:45:55 +0900

    Fix run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,12 @@
 CONTAINER_NAME=akawashiro_site_container
 IMAGE_NAME=akawashiro_site_image
 
-EXITING_CONTAINER_ID=$(sudo podman ps -aqf "name=akawashiro_site_container")
+EXITING_CONTAINER_ID=$(podman ps -aqf "name=akawashiro_site_container")
 if [ -n "${EXITING_CONTAINER_ID}" ]; then
-  sudo podman stop ${EXITING_CONTAINER_ID}
-  sudo podman rm ${EXITING_CONTAINER_ID}
+  podman stop ${EXITING_CONTAINER_ID}
+  podman rm ${EXITING_CONTAINER_ID}
 fi
-sudo podman pull docker.io/library/nginx:latest
-sudo podman image build --network host . -t ${IMAGE_NAME}
-sudo podman run -d --name ${CONTAINER_NAME} ${IMAGE_NAME}
-CONTAINER_IP_ADDR=$(sudo podman inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_NAME})
-echo "Access to http://${CONTAINER_IP_ADDR}"
+podman pull docker.io/library/nginx:latest
+podman image build --network host . -t ${IMAGE_NAME}
+podman run --publish 8080:80/tcp -d --name ${CONTAINER_NAME} ${IMAGE_NAME}
+echo "Access to http://localhost:8080/"
